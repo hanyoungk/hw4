7a54060c65e0a2e8ef14d754c9387b121f6f753e
hanyoungk <hanykim029@gmail.com>
2023-03-13 14:37:23 -0700

    tree traversals done

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -1,6 +1,6 @@
 #ifndef RECCHECK
 //if you want to add any #includes like <iostream> you must do them here (before the next endif)
-
+#include <algorithm>
 #endif
 
 #include "equal-paths.h"
@@ -8,11 +8,34 @@ using namespace std;
 
 
 // You may add any prototypes of helper functions here
+int height(Node* root)
+{
+    if(root == nullptr) { //base case
+        return 0; //reached end
+    }
+    if(root->left && root->right) { //parent has both children
+        int height1 = height(root->left); //height of left child
+        int height2 = height(root->right); //height of right child
+        return 1 + max(height1,height2); //return the max
+    }
+    else if(root->left){
+        return 1 + height(root->left);
+    }
+    else if(root->right) {
+        return 1 + height(root->right);
+    }
 
+}
 
 bool equalPaths(Node * root)
 {
-    // Add your code below
+    if(root == nullptr) {
+        return true;
+    }
+
+    int left = 1 + height(root->left);
+    int right = 1 + height(root->right);
+    return (left == right);
 
 }
 
